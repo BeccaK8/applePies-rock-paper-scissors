c908847e09ef00857b4c2e0cf67e14574c058720
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2023-11-28 10:59:27 -0600

    adds initial js notes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-console.log('js is linked and ready')
+/*------ plan of action ------*/
+// Identify and initialize our state variables
+// (pertain to the state of our game, usually they are the initial starting points of our elements)
+// (our starting values) 
+
+// Code main render() function & our renderResults()
+// (these are controllers that update the view based on user input)
+
+// Code the click event listener
+// Code the win logic
+// Update our renderResults() after we have win logic (to add border to winner)
+
+// Code a countdown timer
+// Add audio to the countdown to improve user experience
+
+
+
+/*------ constants ------*/
+
+// list of choices
+const RPS_LOOKUP = {
+    r: {img: 'imgs/rock.png', beats: 's'},
+    p: {img: 'imgs/paper.png', beats: 'r'}, 
+    s: {img: 'imgs/scissors.png', beats: 'p'}
+};
+
+
+/*------ cached element references ------*/
+const pResultEl = document.getElementById('p-result');
+const cResultEl = document.getElementById('c-result');
+
+const countdownEl = document.getElementById('countdown');
+
+
+/*------ App's initial state variables ------*/
+// some things will change as the game proceeds
+let scores;
+// scores will be object keys (p = player score, c = computer score, t = tie)
+
+let results;
+
+let winner;
+
+
+
+/*------ functions ------*/
+
+
+/*------ event listeners ------*/
